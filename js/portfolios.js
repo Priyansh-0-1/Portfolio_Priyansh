6558edbdaa5ebe4f634f4c8ae26149e9ad9d609a
Priyansh Liya <154884246+Priyansh-0-1@users.noreply.github.com>
2025-06-16 12:57:53 +0530

    Port

---
+++ js/portfolios.js
@@ -0,0 +1,247 @@
+var portfolio_data = [
+	{
+		id: 'projects-pizza-wizza',
+		images: [
+			{
+				src: './images/PizzaWizzaSlider/01.png',
+				thumb: './images/PizzaWizzaSlider/01.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/02.png',
+				thumb: './images/PizzaWizzaSlider/02.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/03.png',
+				thumb: './images/PizzaWizzaSlider/03.png',
+				subHtml:
+					'<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/04.png',
+				thumb: './images/PizzaWizzaSlider/04.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/05.png',
+				thumb: './images/PizzaWizzaSlider/05.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/06.png',
+				thumb: './images/PizzaWizzaSlider/06.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/07.png',
+				thumb: './images/PizzaWizzaSlider/07.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/PizzaWizzaSlider/08.png',
+				thumb: './images/PizzaWizzaSlider/08.png',
+				subHtml:
+					'<h4></h4><p></p>',
+			},
+		],
+	},
+	{
+		id: 'chungi-yoo',images: [
+			{
+				src: './images/ChungiYoo/01.png',
+				thumb: './images/ChungiYoo/01.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ChungiYoo/02.png',
+				thumb: './images/ChungiYoo/02.png',
+				subHtml: '<h4> </h4><p></p>',
+			},
+			{
+				src: './images/ChungiYoo/03.png',
+				thumb: './images/ChungiYoo/03.png',
+				subHtml: '<h4></h4><p </p>',
+			},
+			{
+				src: './images/ChungiYoo/04.png',
+				thumb: './images/ChungiYoo/04.png',
+				subHtml: '<h4></h4><p> </p>',
+			},
+			{
+				src: './images/ChungiYoo/05.png',
+				thumb: './images/ChungiYoo/05.png',
+				subHtml: '<h4></h4><p> </p>',
+			},
+		],
+	},
+	{
+		id: 'jdesign',images: [
+			{
+				src: './images/JDesign/01.png',
+				thumb: './images/JDesign/01.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/JDesign/02.png',
+				thumb: './images/JDesign/02.png',
+				subHtml: '<h4></h4><p> </p>',
+			},
+			{
+				src: './images/JDesign/03.png',
+				thumb: './images/JDesign/03.png',
+				subHtml: '<h4> </h4><p></p>',
+			},
+			{
+				src: './images/JDesign/04.png',
+				thumb: './images/JDesign/04.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/JDesign/05.png',
+				thumb: './images/JDesign/05.png',
+				subHtml: '<h4> </h4><p></p>',
+			},
+			{
+				src: './images/JDesign/06.png',
+				thumb: './images/JDesign/06.png',
+				subHtml: '<h4> </h4><p></p>',
+			},
+		],
+	},
+
+	{
+		id: 'lagunitas',images: [
+			{
+				src: './images/Lagunitas/01.png',
+				thumb: './images/Lagunitas/01.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/Lagunitas/02.png',
+				thumb: './images/Lagunitas/02.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/Lagunitas/03.png',
+				thumb: './images/Lagunitas/03.png',
+				subHtml: '<h4></h4><p </p>',
+			},
+			{
+				src: './images/Lagunitas/04.png',
+				thumb: './images/Lagunitas/04.png',
+				subHtml: '<h4></h4><p> </p>',
+			},
+			{
+				src: './images/Lagunitas/05.png',
+				thumb: './images/Lagunitas/05.png',
+				subHtml: '<h4></h4><p> </p>',
+			},
+		],
+	},
+	{
+		id: 'earth-parallex',images: [
+			{
+				src: './images/EarthParallex/01.png',
+				thumb: './images/EarthParallex/01.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/EarthParallex/02.png',
+				thumb: './images/EarthParallex/02.png',
+				subHtml: '<h4></h4><p>  </p>',
+			},
+			{
+				src: './images/EarthParallex/03.png',
+				thumb: './images/EarthParallex/03.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/EarthParallex/04.png',
+				thumb: './images/EarthParallex/04.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+		],
+	},
+	{
+		id: 'expense-management-tracker',
+		images: [{
+				src: './images/ExpenseTrackerSlider/01.png',
+				thumb: './images/ExpenseTrackerSlider/01.png',
+				subHtml:
+					'<h4></h4><p></p>',
+			}
+			
+		],
+	},
+	{
+		id: 'prajs-ngo-project',images: [
+			{
+				src: './images/PrajsNgoSlider/01.jpg',
+				thumb: './images/PrajsNgoSlider/01.jpg',
+				subHtml: '<p></p>',
+			}
+		],
+	},
+	{
+		id: 'splice-awwwards-project',
+		images: [{
+				src: './images/SpliceSlider/01.jpg',
+				thumb: './images/SpliceSlider/01.jpg',
+				subHtml:
+					'<h4></h4><p></p>',
+			}
+		],
+	},
+	{
+		id: 'thierrychopain-portfolio-project',
+		images: [{
+				src: './images/ThierrySlider/01.png',
+				thumb: './images/ThierrySlider/01.png',
+				subHtml:
+					'<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/02.png',
+				thumb: './images/ThierrySlider/02.png',
+				subHtml:
+					'<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/03.png',
+				thumb: './images/ThierrySlider/03.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/04.png',
+				thumb: './images/ThierrySlider/04.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/05.png',
+				thumb: './images/ThierrySlider/05.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/06.png',
+				thumb: './images/ThierrySlider/06.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/07.png',
+				thumb: './images/ThierrySlider/07.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/08.png',
+				thumb: './images/ThierrySlider/08.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+			{
+				src: './images/ThierrySlider/09.png',
+				thumb: './images/ThierrySlider/09.png',
+				subHtml: '<h4></h4><p></p>',
+			},
+		],
+	},
+];
